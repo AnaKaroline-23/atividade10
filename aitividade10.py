715f812f782095bd855e0ed2c100d4715cd6cf81
AnaKaroline-23 <anakarolinesoraes.23@gmail.com>
2024-09-11 14:26:44 -0300

    programa que informa se um numero é impar ou par

---
+++ aitividade10.py
@@ -0,0 +1,7 @@
+# Crie um programa que receba um número inteiro e informe se ele é par ou ímpar.
+numero = int(input("digite seu numero :"))
+if numero % 2:
+    print ("impar")
+else:
+    print ("par")
+    
